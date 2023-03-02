b72303de5ab37ad00e6097e72276fc260baef7d6
Documenter.jl <documenter@juliadocs.github.io>
2023-03-02 07:39:19 +0000

    build based on 08803fc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SmallDatasetMaker","category":"page"},{"location":"#SmallDatasetMaker","page":"Home","title":"SmallDatasetMaker","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SmallDatasetMaker.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SmallDatasetMaker]","category":"page"}]
+}
