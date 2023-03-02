bafc8a4568148828ff62bc50e654d044e85770c0
okatsn <okatsn@gmail.com>
2023-03-02 15:47:02 +0800

    Files copied from SWCDatasets v0.2.5

--- src/SmallDatasetMaker.jl
+++ src/SmallDatasetMaker.jl
@@ -2,4 +2,21 @@ module SmallDatasetMaker
 
 # Write your package code here.
 
+global __datasets = nothing
+
+using OkFiles
+
+using CSV,DataFrames
+include("datasets.jl")
+
+include("datadir.jl")
+
+using CodecZlib,Dates
+import PrettyTables
+include("compress.jl")
+export SourceData, compress_save, compress_save!
+
+
+include("decompress.jl")
+
 end
