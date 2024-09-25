dc0e0fa1bf66ad10cca459aa831d927bdae503dc
okatsn <okatsn@gmail.com>
2024-09-25 03:26:40 +0000

    Separate the last-row selection algorithm as `lastestentry`.

--- src/gc.jl
+++ src/gc.jl
@@ -1,11 +1,20 @@
 """
-`cleantable(mod::Module)` remove redundant entries of the `dataset_table(mod)` **and overwrite** `data/docs/datasets.csv`. Use with caution and verify before commit & push.
+`cleantable(mod::Module)` remove redundant entries of the `dataset_table(mod)` **and overwrite** `data/doc/datasets.csv`. Use with caution and verify before commit & push.
 
+# Example
 
+```julia
+using SmallDatasetMaker, YourDatasets
+SmallDatasetMaker.cleantable(YourDatasets)
+```
 """
-function cleantable(mod)
+function cleantable(mod::Module)
     reftablepath = dataset_table(mod)
     df = CSV.read(reftablepath, DataFrame)
-    df_lite = combine(groupby(df, [:PackageName, :Dataset]; sort = false), last)
+    df_lite = lastestentry(df)
     CSV.write(reftablepath, df_lite)
+end
+
+function lastestentry(df)
+    combine(groupby(df, [:PackageName, :Dataset]; sort=false), last)
 end
